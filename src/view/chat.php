e8d1b65dba694b20119c940d1cd185d77a616d7d
matauangcina <yuan.iskandar@binus.ac.id>
2022-12-01 16:19:30 +0700

    test

--- src/view/chat.php
+++ src/view/chat.php
@@ -7,8 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/chat.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.js" integrity="sha512-CX7sDOp7UTAq+i1FYIlf9Uo27x4os+kGeoT7rgwvY+4dmjqV0IuE/Bl5hVsjnQPQiTOhAX1O2r2j5bjsFBvv/A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
-    <!-- <script src="../js/component.js" defer></script> -->
     <script src="../js/chat.js" defer></script>
     <title>TicTALK</title>
 </head>
@@ -50,14 +48,6 @@
                     </div>
                     <div class="bx bx-heart"></div>
                 </div>
-                <!-- <div class="wrapper">
-                    <img src="../../assets/female.png" alt="">
-                    <div class="info">
-                        <h3>Eod Nhoj</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum fugit natus, voluptatem veniam molestias ipsum consequatur, dignissimos molestiae sint vero eius aperiam illo aliquid quaerat vel! Quibusdam, doloribus quasi.</p>
-                    </div>
-                    <div class="bx bx-heart"></div>
-                </div> -->
             </div>
         </div>
         <div class="chat">
@@ -79,18 +69,6 @@
                 <div class="chat-content">
                     <div class="chat-content-list">
                         <div class="bubble-chat user">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum fugit natus, voluptatem veniam molestias ipsum consequatur, dignissimos molestiae sint vero eius aperiam illo aliquid quaerat vel! Quibusdam, doloribus quasi.</div>
-                        <!-- <div class="bubble-chat friend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, vitae harum! Itaque omnis dignissimos quibusdam expedita voluptates, nulla laborum enim non autem similique saepe! Eius illo perspiciatis natus repellendus ipsa.</div>
-                        <div class="bubble-chat friend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore facilis sit rem quisquam quod, laboriosam voluptas illum quaerat eligendi in numquam quidem. Libero eaque voluptatibus unde doloribus veniam ab minima!</div>
-                        <div class="bubble-chat user">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt obcaecati aspernatur harum dolorum quia non ab! Reprehenderit sequi amet officiis voluptatibus asperiores quaerat necessitatibus? Eveniet et maiores maxime nemo rerum.</div>
-                        <div class="bubble-chat user">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nostrum nobis pariatur, sit consequuntur iste aliquam? Maiores, unde laudantium itaque non doloribus error sed cupiditate voluptatibus, odio atque, veniam exercitationem?</div>
-                        <div class="bubble-chat friend">testing</div>
-                        <div class="bubble-chat friend">testing</div>
-                        <div class="bubble-chat friend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, vitae harum! Itaque omnis dignissimos quibusdam expedita voluptates, nulla laborum enim non autem similique saepe! Eius illo perspiciatis natus repellendus ipsa.</div>
-                        <div class="bubble-chat friend">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore facilis sit rem quisquam quod, laboriosam voluptas illum quaerat eligendi in numquam quidem. Libero eaque voluptatibus unde doloribus veniam ab minima!</div>
-                        <div class="bubble-chat user">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt obcaecati aspernatur harum dolorum quia non ab! Reprehenderit sequi amet officiis voluptatibus asperiores quaerat necessitatibus? Eveniet et maiores maxime nemo rerum.</div>
-                        <div class="bubble-chat user">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nostrum nobis pariatur, sit consequuntur iste aliquam? Maiores, unde laudantium itaque non doloribus error sed cupiditate voluptatibus, odio atque, veniam exercitationem?</div>
-                        <div class="bubble-chat friend">testing</div>
-                        <div class="bubble-chat friend">testing</div> -->
                     </div>
                 </div>
                 <div class="chat-bar">
